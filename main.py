22bcfc0193ebd047c7702d4603d801c4a59c8fab
Novice1hao <98198868+Novice1hao@users.noreply.github.com>
2022-08-25 10:27:14 +0800

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,9 @@ from wechatpy.client.api import WeChatMessage, WeChatTemplate
 import requests
 import os
 import random
+import time,httplib
 
-today = datetime.now()
+today =  getBeijinTime()
 str_today = str(today)
 start_date = os.environ['START_DATE']
 city = os.environ['CITY']
@@ -20,6 +21,25 @@ user_id = os.environ["USER_ID"]
 user_id1 = os.environ["USER_ID1"]
 template_id = os.environ["TEMPLATE_ID"]
 
+def getBeijinTime():
+   try:
+     conn = httplib.HTTPConnection("www.beijing-time.org")
+     conn.request("GET", "/time.asp")
+     response = conn.getresponse()
+     print response.status, response.reason
+     if response.status == 200:
+       result = response.read()
+       data = result.split("\r\n")
+       year = data[1][len("nyear")+1 : len(data[1])-1]
+       month = data[2][len("nmonth")+1 : len(data[2])-1]
+       day = data[3][len("nday")+1 : len(data[3])-1]
+       #wday = data[4][len("nwday")+1 : len(data[4])-1]
+       hrs = data[5][len("nhrs")+1 : len(data[5])-1]
+       minute = data[6][len("nmin")+1 : len(data[6])-1]
+       sec = data[7][len("nsec")+1 : len(data[7])-1]
+       beijinTimeStr = "%s/%s/%s %s:%s:%s" % (year, month, day, hrs, minute, sec)
+       beijinTime = time.strptime(beijinTimeStr, "%Y/%m/%d %X")
+       return beijinTim
 
 def get_weather():
   url = "http://autodev.openspeech.cn/csp/api/v2.1/weather?openId=aiuicus&clientType=android&sign=android&city=" + city
