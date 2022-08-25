dbe21a530c64ae9552c55e4e0846be005605b681
Novice1hao <98198868+Novice1hao@users.noreply.github.com>
2022-08-25 11:03:34 +0800

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ user_id = os.environ["USER_ID"]
 user_id1 = os.environ["USER_ID1"]
 template_id = os.environ["TEMPLATE_ID"]
                          
-today1 = datetime.now()
+today1 = datetime.now().strftime('"%Y-%m-%dT%H:%M:%S.%fZ')
 str_today1 = str(today1)            
 _date = datetime.strptime(str_today1, "%Y-%m-%dT%H:%M:%S.%fZ")
 local_time = _date + timedelta(hours=8)
